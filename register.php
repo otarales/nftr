a9d20d2c866af768daf8a4fb3bdc663debc184b9
otarales <90070450+otarales@users.noreply.github.com>
2022-04-18 14:06:11 +0100

    Update register.php

--- register.php
+++ register.php
@@ -94,45 +94,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
  - nftresourcex.com</title>
 </head>
 <body>
-    <nav class="navbar navbar-fixed-top navbar-default" id="navbar-site">
-  <div class="container">
-    <div class="navbar-header">
-      
-        <a class="navbar-brand site-logo-img" href="/"><img src="../images/Logomaker.png" class="img-responsive site-logo"/></a>
-      
-
-      <!-- Dropdown menu toggle button in responsive mode -->
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <!-- Info for screen readers only -->
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      
-    </div>
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      
-
-      <!-- Right side of navbar -->
-      
-        
-          <ul class="nav navbar-nav navbar-right">
-            <li>
-              <a id="top-register-link" class="register-link" href="register.php"><span><i class="fa fa-check-square-o"></i>
-              
-                Sign up 
-              
-                </span>
-              </a>
-            </li>
-            <li><a id="top-login-link" href="login.php"><i class="fa fa-user"></i>&nbsp;Log in</a></li>
-          </ul>
-        
-      
-    </div>
-  </div>
-</nav>
+     <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><img src="images/Logomaker.png" width="55px" alt="" srcset=""></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  </header>
 
     <div class="wrapper">
         <h2>Register a new account</h2>
